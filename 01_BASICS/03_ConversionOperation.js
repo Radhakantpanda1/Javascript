8e9f099d20b36849185e0b62c6fadaffda8cd724
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-10-31 09:12:12 +0000

    conversion of datatypes

---
+++ 01_BASICS/03_ConversionOperation.js
@@ -0,0 +1,60 @@
+/*
+let score=33;
+let new_score="33";
+console.log(typeof score);
+console.log(typeof new_score);
+let valueInNumber=Number(new_score);
+console.log(typeof valueInNumber);
+*/
+/*
+let score=true;
+//let new_score="33";
+console.log(typeof score);
+//console.log(typeof new_score);
+let valueInNumber=Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+*/
+/*
+let score=null;
+//let new_score="33";
+console.log(typeof score);
+//console.log(typeof new_score);
+let valueInNumber=Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+*/
+/*
+let score="36ab";
+//let new_score="33";
+console.log(typeof score);
+//console.log(typeof new_score);
+let valueInNumber=Number(score);
+console.log(valueInNumber);//NaN
+console.log(typeof valueInNumber);
+*/
+/*
+let score=undefined;
+//let new_score="33";
+console.log(typeof score);
+//console.log(typeof new_score);
+let valueInNumber=Number(score);
+console.log(valueInNumber);//NaN
+console.log(typeof valueInNumber);
+*/
+/*
+let isLoggedIn= 0;
+let booleanIsLogged= Boolean(isLoggedIn);
+console.log(booleanIsLogged);
+isLoggedIn=1;
+booleanIsLogged= Boolean(isLoggedIn);
+console.log(booleanIsLogged);
+isLoggedIn="";
+booleanIsLogged= Boolean(isLoggedIn);
+console.log(booleanIsLogged);
+isLoggedIn="RADHAKANT";
+booleanIsLogged= Boolean(isLoggedIn);
+console.log(booleanIsLogged);
+*/
+//  1--->TRUE   0--->FALSE
+// ""/empty string ---> false  "radhakant" --->true
