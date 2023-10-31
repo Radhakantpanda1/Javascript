9866876b53360d197c586c85e93f690dbfa125da
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-10-31 09:35:50 +0000

    operations

--- 01_BASICS/03_ConversionOperation.js
+++ 01_BASICS/03_ConversionOperation.js
@@ -58,3 +58,48 @@ console.log(booleanIsLogged);
 */
 //  1--->TRUE   0--->FALSE
 // ""/empty string ---> false  "radhakant" --->true
+
+// ******************************* opearations *****************************************
+/*
+let value=3;
+let negValue=-value;
+console.log(value);// 3
+console.log(negValue); // -3
+*/
+// basic maths
+let str1="hello";
+let str2=" radhakant";
+let str3=str1+str2;
+//console.log(str3);// hello radhakant
+
+/*
+console.log(1+2+3);//simple addition 
+console.log("1"+2+3);// pehle string aaya islie saara string me convert ho gayya
+console.log(1+2+"3");// pehle (1+2) evaluate hua phir convert hgaya string me
+console.log(1+"2"+3);// string me convert hogaya
+*/
+// prefix and postfix operators  (study from mdn docs)
+// The increment (++) operator increments (adds one to) its operand and returns the value before or after the increment, depending on where the operator is placed.
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+/*
+// prefix
+const y = ++x;
+// x is 4; y is 4
+
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 4n
+*/
+/* 
+postfix
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+
+*/
+
