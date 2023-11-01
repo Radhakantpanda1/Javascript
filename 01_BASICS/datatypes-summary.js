778fb7d33ffa5167baaff68013159b88743f9273
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-01 01:49:41 +0000

    heap and stack

--- 01_BASICS/datatypes-summary.js
+++ 01_BASICS/datatypes-summary.js
@@ -15,6 +15,7 @@ objects
 functions
 */
 // javascript is dynamically typed 
+/*
 const id=Symbol('1436');
 const newId=Symbol('1436');
 console.log(typeof id);// symbol
@@ -22,3 +23,29 @@ console.log(typeof newId);//symbol
 console.log(id===newId);// false
 
 console.log(typeof 25.36);// number
+*/
+
+// stack(primitive) heap(non-primitive)
+let myName="Radhakant";
+let myAnotherName=myName;
+console.log(myAnotherName);//Radhakant
+myAnotherName="sinu";
+console.log(myAnotherName);//sinu
+console.log(myName);//Radhakant
+
+let myData={
+    myName:"radhakant",
+    pswd:"kkklll"
+}
+let myNewData=myData;
+
+console.log(myData.myName);
+console.log(myData.pswd);
+console.log(myNewData.myName);
+console.log(myNewData.pswd);
+
+myData.myName="sinu";
+console.log(myData.myName);
+console.log(myData.pswd);
+console.log(myNewData.myName);
+console.log(myNewData.pswd);
