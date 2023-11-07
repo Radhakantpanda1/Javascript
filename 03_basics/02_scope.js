716bad0fed2c6c279f159b78d0d7a744240a8101
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-07 14:20:26 +0000

    more on scopes

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -11,3 +11,35 @@ if(true){
  //   console.log(y);//ReferenceError: y is not defined
     console.log(z);//35
     // this is why it is advised to not to use var.
+
+   // ************ more on scope ******************
+   function one(){
+    const userName="Radhakant Panda"
+    function two(){
+        const website="youtube"
+        console.log(userName);
+    }
+        //console.log(website);//ReferenceError: website is not defined
+        two();
+   }
+   one();
+
+   // another example
+   if(true){
+    const userName="Radhakanr Panda"
+    if(userName==="Radhakanr Panda"){
+        const website=" ytb"
+        console.log(userName+website);
+    }
+   }
+
+   // +++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++++++++
+   addOne(9);
+   function addOne(num){
+   console.log( num+1) ;
+   }
+  // addTwo(15);//ReferenceError: Cannot access 'addTwo' before initialization
+   const addTwo=function(num){
+    console.log( num+2) ;
+   }
+   addTwo(15);
