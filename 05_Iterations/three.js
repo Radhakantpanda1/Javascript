64c2a4d32d63c66b714167402f1b039056cfa431
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-09 17:26:35 +0000

    higher order array loops

---
+++ 05_Iterations/three.js
@@ -0,0 +1,14 @@
+// array specific loops
+
+// ** for of loop
+// [""."","",....]
+// [{},{},{},....]
+/*
+for (const iterator of object) {
+    
+}
+*/
+const array = [1,2,3,4,5,6,7,8,9,10];
+for (const num of array) {
+    console.log(num);
+}
