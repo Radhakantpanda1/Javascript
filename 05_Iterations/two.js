07b8ed63c3ece4e871c20ea5decc6abf031d182a
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-09 17:14:58 +0000

    while and do-while loop

---
+++ 05_Iterations/two.js
@@ -0,0 +1,17 @@
+//while loop
+/*
+let i=0;
+while (i<10) {
+    console.log("hello");
+    i++;
+}
+*/
+// do-while
+/*
+let x=0;
+do{
+    console.log("hello radhakant");
+    x++;
+}while(x<10);
+*/
+// in worst case it runs for atleast one.
