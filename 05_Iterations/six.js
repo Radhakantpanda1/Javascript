b29a601e4ed0dc2e0ccb21a31c3166badfd33530
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-10 09:52:00 +0000

    filter

---
+++ 05_Iterations/six.js
@@ -0,0 +1,65 @@
+// note : for each loop doesn't return any value.
+const myNumber=[3,4,5,6,7,8,9,10];
+let numContainer=myNumber.filter((item) => item>5);// no need to use return keyword bcoz of implicit return
+console.log(numContainer);// [ 6, 7, 8, 9, 10 ]
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+  let userWants=books.filter((bk) => bk.edition >= 2000);
+  // console.log(userWants);
+  /*
+  *********   OUTPUT ++++++++++++++++
+  [
+  { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+  {
+    title: 'Book Two',
+    genre: 'Non-Fiction',
+    publish: 1992,
+    edition: 2008
+  },
+  {
+    title: 'Book Three',
+    genre: 'History',
+    publish: 1999,
+    edition: 2007
+  },
+  {
+    title: 'Book Four',
+    genre: 'Non-Fiction',
+    publish: 1989,
+    edition: 2010
+  },
+  {
+    title: 'Book Five',
+    genre: 'Science',
+    publish: 2009,
+    edition: 2014
+  },
+  { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+  {
+    title: 'Book Eight',
+    genre: 'Science',
+    publish: 2011,
+    edition: 2016
+  }
+]
+*/
+
+
+const Userneeds=books.filter((bk) => {return bk.genre==='Fiction'});
+// console.log(Userneeds);
+/*
+[
+  { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+  { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 }
+]
+*/
