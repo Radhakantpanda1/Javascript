03fe5bb8204a747b5e6492e065c41f2bc0bba375
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-11-11 08:04:36 +0000

    basics revision

---
+++ 00_REVISION/01_rev_basics.js
@@ -0,0 +1,92 @@
+// word v/s keyword   
+// random collection of alphabate --> word
+// collection of alphabate which has some meaning in javascript--> keyword
+
+//           *****************+++++++++++++++++
+
+// var let const
+// var is used to ceate variable (which can store data ) but is independent of block scope
+// let is also used to create variables ,but they obey block scope
+// const is used to create a varible whose value can't be overwritten
+ // *************************************************************************
+
+ // hoisting - In JS variables and functions are hoisted , which means their declaration is moved to the top of code
+
+
+ // primitive- string , number , boolean , null , undefined
+ // reference-  [] , () , {}  
+ //        *******************************************************************************
+
+ // if  else else-if
+ /*
+ if (condition) {
+    
+ }
+ */
+/*
+else{
+
+}
+*/
+/*
+else if (condition) {
+    
+} 
+*/
+
+// ********************************************************************************************************************
+
+// loops - for and while
+/*
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+*/
+/*
+while (condition) {
+    
+}
+*/
+for(let i=0;i<10;i++){
+    //console.log(i);
+}
+let x=0;
+while(x<10){
+   // console.log(x);
+    x++;
+}
+
+
+// ++++++++++++++++++++++++++++++++++++ function ++++++++++++++++++++++++++++++++++++++++++++++++++
+function name(params) {
+    
+}
+let userDetailsPrinter=function (name,age,id){
+    /*
+   console.log(`username=${name}`);
+   console.log(`Age=${age}`);
+   console.log(`Id=${id}`);
+   */
+}
+
+userDetailsPrinter("Radhakant panda",21,"radhakant@google");
+
+
+// *******************************************************arrays+++++++++++++++++++++++++++++++++++++++++++
+
+// used to store multiple values
+let array=[1,2,3,"superman"];
+for(let i=0;i<4;i++){
+    console.log(array[i]);
+}
+// push pop shift unshift slice splice
+
+
+// *************************************     objects    *******************************************
+var userDetails={}// empty object
+var employeeDetails={
+    employeeName:"saransh",
+    employeeAge:45,
+    employeeId:"saransh@sarasarrr.com"
+}// this is a filled object
